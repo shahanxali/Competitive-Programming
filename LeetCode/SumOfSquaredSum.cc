5ebaa33204af47ea3c25eee0c0385cfbdd9bd59a
shahanxali <shahanali252002@gmail.com>
2024-03-28 21:38:50 +0530

    lc

---
+++ LeetCode/SumOfSquaredSum.cc
@@ -0,0 +1,21 @@
+//Question: https://leetcode.com/problems/sum-of-square-numbers/
+//Level: Medium
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a = 0, b = sqrt(c); 
+
+        while (a <= b) {
+            long long d = a * a + b * b;
+            if (d == c) {
+                return true;
+            } else if (d < c) {
+                a++;
+            } else {
+                b--;
+            }
+        }
+        return false;
+    }
+};
