17361953721459fbdfe9059edba10c436d4b045a
shahanxali <shahanali252002@gmail.com>
2024-05-23 10:28:58 +0530

    daily

---
+++ LeetCode/TheNumberOfBeautifulSubsets.cc
@@ -0,0 +1,54 @@
+//Question: https://leetcode.com/problems/the-number-of-beautiful-subsets/
+//Level: Medium
+
+class Solution {
+public:
+    
+    int ans;
+    map<int, int> rec;
+    
+        void recursion(int pos, int n, vector<int> &tmp, vector<int>& nums, int k) {
+            
+            
+        if (pos == n) {
+            ans++;
+            return;
+        }
+            
+        if (rec[nums[pos] - k] == 0) {
+            
+            rec[nums[pos]]++;
+            
+            recursion(pos + 1, n, tmp, nums, k);
+            
+            rec[nums[pos]]--;
+        
+        }   
+            
+        recursion(pos + 1, n, tmp, nums, k);   
+            
+    }
+    
+    int beautifulSubsets(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        
+        ans = 0;
+        vector<int> tmp;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+        
+            rec[nums[i]]++;
+            
+            recursion(i+1, n, tmp, nums, k);
+            
+            rec[nums[i]]--;
+        
+        }
+    
+        return ans;
+    
+    }
+    
+};
