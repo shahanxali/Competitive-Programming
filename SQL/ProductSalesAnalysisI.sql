8195b78b75eb960d53e11f6b831da8ae85dd3107
shahanxali <shahanali252002@gmail.com>
2024-08-09 20:20:36 +0530

    db

---
+++ SQL/ProductSalesAnalysisI.sql
@@ -0,0 +1,8 @@
+--Question: https://leetcode.com/problems/product-sales-analysis-i/
+--Level: Easy
+
+# Write your MySQL query statement below
+
+SELECT p.product_name, s.year, s.price FROM Sales s JOIN Product p 
+ON s.product_id = p.product_id;
+
